4ac187407449ca0e8755b0d77e364e371f46d95b
nuryokusan <nuryokusan@gmail.com>
2016-05-11 12:57:19 +0200

    persist - fix failing tests

--- test/songcontest/test_persist.clj
+++ test/songcontest/test_persist.clj
@@ -7,14 +7,14 @@
 (defmulti create-table-ddl identity)
   
 (defmethod create-table-ddl :contest [table]  
-  (jdbc/create-table-ddl table [:id "BIGINT PRIMARY KEY AUTO_INCREMENT"]
-                               [:name "VARCHAR NOT NULL"]
-                               [:phase "VARCHAR NOT NULL"]))
+  (jdbc/create-table-ddl table [[:id "BIGINT PRIMARY KEY AUTO_INCREMENT"]
+                                [:name "VARCHAR NOT NULL"]
+                                [:phase "VARCHAR NOT NULL"]]))
 
 (defmethod create-table-ddl :nomination [table]  
-  (jdbc/create-table-ddl table [:id "BIGINT PRIMARY KEY AUTO_INCREMENT"]
-                               [:name "VARCHAR NOT NULL"]
-                               [:phase "VARCHAR NOT NULL"]))
+  (jdbc/create-table-ddl table [[:id "BIGINT PRIMARY KEY AUTO_INCREMENT"]
+                                [:name "VARCHAR NOT NULL"]
+                                [:phase "VARCHAR NOT NULL"]]))
 
 (defn create-test-table! [table]
     (jdbc/db-do-commands db/db (create-table-ddl table)))
@@ -42,29 +42,29 @@
   (create-test-table! :contest)
   (is (= (count (persist/read-contest db/db)) 0))
   (persist/create-contest! db/db contest1)
-  (let [read-result (persist/read-contest db/db)]
-    (is (= (count read-result) 1))
-    (is (= (:name (first read-result)) (:name contest1)))
-    (is (= (:phase (first read-result)) (:phase contest1)))))
+  (let [created (persist/read-contest db/db)]
+    (is (= (count created) 1))
+    (is (= (:name (first created)) (:name contest1)))
+    (is (= (:phase (first created)) (:phase contest1)))))
 
 (deftest update-contest []
   (create-test-table! :contest)
   (persist/create-contest! db/db contest1)
-  (let [read-result-create (persist/read-contest db/db)
-        id-create (:id (first read-result-create))]
-    (persist/update-contest! db/db id-create contest2)
-    (let [read-result-update (persist/read-contest db/db)
-          {:keys [:id :name :phase]} (first read-result-update)]
-      (is (= (count read-result-update) 1))
-      (is (= id id-create))
+  (let [created (persist/read-contest db/db)
+        id-created (:id (first created))]
+    (persist/update-contest! db/db id-created contest2)
+    (let [updated (persist/read-contest db/db)
+          {:keys [:id :name :phase]} (first updated)]
+      (is (= (count updated) 1))
+      (is (= id id-created))
       (is (= name (:name contest2)))
       (is (= phase (:phase contest2))))))
 
 (deftest delete-contest []
   (create-test-table! :contest)
   (persist/create-contest! db/db contest1)
-  (let [read-result (persist/read-contest db/db)
-        id (:id (first read-result))]
+  (let [created (persist/read-contest db/db)
+        id (:id (first created))]
     (persist/delete-contest! db/db id)
     (is (= (count (persist/read-contest db/db)) 0))))
 
